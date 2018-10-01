9fd61b000e7e184dfe87e6e34fc991a96a106d6e
feriadicandra <ferizacky@gmail.com>
2018-10-01 20:57:16 +0700

    Initial commit

---
+++ app/src/main/java/frans/biodataku/MainActivity.java
@@ -0,0 +1,44 @@
+package frans.biodataku;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void cellphone(View view) {
+        Uri uri = Uri.parse("tel:00895415021514");
+        Intent it = new Intent (Intent.ACTION_VIEW, uri );
+        startActivity(it);
+
+    }
+
+    public void showMap(View view) {
+        Uri uri = Uri.parse("geo:-6.975282,110.4045053,17=");
+        Intent it = new Intent (android.content.Intent.ACTION_VIEW, uri );
+        startActivity(it);
+
+    }
+
+    public void email(View view) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[] {"gregoriusfranjaya@gmail.com"});
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Email dari Aplikasi Android");
+
+        try {
+            startActivity(Intent.createChooser(intent, "Ingin Mengirim Email ?"));
+        } catch (android.content.ActivityNotFoundException ex) {
+
+        }
+    }
+}
+
